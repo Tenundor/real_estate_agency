de0413b045e363d87aab1363fd0429618550e7db
Antonio <Tenundor@gmail.com>
2021-02-05 16:04:37 +0300

    Added blank="True" to liked_by

--- property/models.py
+++ property/models.py
@@ -30,7 +30,7 @@ class Flat(models.Model):
     new_building = models.NullBooleanField("Новостройка", db_index=True)
 
     liked_by = models.ManyToManyField(User, related_name="liked_flats",
-                                      verbose_name="Кто лайкнул")
+                                      verbose_name="Кто лайкнул", blank=True)
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
